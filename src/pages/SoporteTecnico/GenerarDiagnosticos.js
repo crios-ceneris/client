e95d6072e4e6992b53afde53523f93ad7488a1d4
ceva <cebada.v1@gmail.com>
2024-02-27 18:13:29 -0500

    Actualizacion en la tabla mostrar diagnostico operativo

--- src/pages/SoporteTecnico/GenerarDiagnosticos.js
+++ src/pages/SoporteTecnico/GenerarDiagnosticos.js
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function GenerarDiagnosticos() {
-  return(
-    <div className='p-2'>
-      {/* Título de la tabla */}
-      <div className='d-grid gap-2'>
-        <div className='p-1 bg-primary rounded-2 text-center text-white fw-medium'>Generar Diagnósticos</div>
-      </div>
+  const [diagnosticsData, setDiagnosticsData] = useState([]);
+  const [username, setUsername] = useState('');
 
-      {/* Tabla con datos */}
-      <div className='mt-4'>
-        <div className="input-group w-25 mb-4">
-          <span className="input-group-text bg-success text-white">Responsable</span>
-          <input type="text" className="form-control bg-light fw-medium" value="Técnico 1" disabled readOnly/>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/diagnostico'); // Reemplaza con la URL de tu backend
+        const data = await response.json();
+        const storedUsername = localStorage.getItem('username');
+        if (storedUsername) {
+            setUsername(storedUsername);
+        }
+        setDiagnosticsData(data);
+      } catch (error) {
+        console.error(error);
+        // Mostrar un mensaje de error al usuario
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+      <div className='p-2'>
+        {/* Título de la tabla */}
+        <div className='d-grid gap-2'>
+          <div className='p-1 bg-primary rounded-2 text-center text-white fw-medium'>Generar Diagnósticos</div>
         </div>
-        <div className='table-responsive'>
-          <table className='table text-center'>
-            <thead>
+
+        {/* Tabla con datos */}
+        <div className='mt-4'>
+          <div className="input-group w-25 mb-4">
+            <span className="input-group-text bg-success text-white">Responsable</span>
+            <input type="text" className="form-control bg-light fw-medium" disabled readOnly/> <td>{username}</td>
+          </div>
+          <div className='table-responsive'>
+            <table className='table text-center'>
+              <thead>
               <tr>
                 <th scope="col">#</th>
                 <th scope="col">Cliente</th>
@@ -29,35 +52,39 @@ function GenerarDiagnosticos() {
                 <th scope="col">Responsable</th>
                 <th scope="col">Fecha Límite</th>
                 <th scope="col">Estado</th>
-                <th scope="col">Diagnostico</th>
                 <th scope="col">Opciones</th>
                 <th scope="col">Observaciones</th>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>Ceneris EIRL Proyecto Cerro Verde</td>
-                <td>Bomba Gravimétrica</td>
-                <td>SKC</td>
-                <td>XR5000</td>
-                <td>86303</td>
-                <td>04/01/2024</td>
-                <td>Muy Alta</td>
-                <td>Técnico 1</td>
-                <td>06/01/2024</td>
-                <td>Enviado</td>
-                <td>Generar</td>
-                <td>Editar/Eliminar</td>
-                <td>Ver</td>
-              </tr>
-            </tbody>     
-          </table>
+              </thead>
+              <tbody>
+              {diagnosticsData.map((diagnostic, index) => (
+                  <tr key={index}>
+                    <th scope='row'>{index + 1}</th>
+                    <td>{diagnostic.Cliente}</td>
+                    <td>{diagnostic.equipo}</td>
+                    <td>{diagnostic.Marca}</td>
+                    <td>{diagnostic.Modelo}</td>
+                    <td>{diagnostic.Serie}</td>
+                    <td>{diagnostic.fecharecepcion}</td>
+                    <td>{diagnostic.Prioridad}</td>
+                    <td>{diagnostic.Responsable}</td>
+                    <td>{diagnostic.fechalimite}</td>
+                    <td>{diagnostic.estadorevision}</td>
+                    <td>
+                      {/* Agregar botones para editar, eliminar y ver detalles del diagnóstico */}
+                      <button className="btn btn-sm btn-primary"><i className="fa-solid fa-file-pen"></i></button>
+                      <button className="btn btn-sm btn-danger"><i className="fa-solid fa-trash"></i></button>
+                      <button className="btn btn-sm btn-info"><i className="fa-solid fa-circle-info"></i></button>
+                    </td>
+                    <td>{diagnostic.Observaciones}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-
       </div>
-    </div>
-  )
-}
+  );
+};
 
-export default GenerarDiagnosticos
+export default GenerarDiagnosticos;
